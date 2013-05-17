d086d1f1353fb86f2954d83f9b18bc5c5b373a79
Rowan Crawford <wombleton@gmail.com>
2013-05-17 13:57:39 +1200

    Use moment to format dates

--- index.js
+++ index.js
@@ -1,25 +1,18 @@
 var _ = require('underscore'),
-    _s = require('underscore.string'),
+    moment = require('moment'),
     parser = require('./lib/parser');
 
 module.exports = {
-  parse: function(s) {
-    if (_.isString(s) && s) {
-      return parser.parse(s);
-    } else {
-      return {
-        error: "Expected an HL7 message but got '" + s + "'"
-      }
+    parse: function(s) {
+        if (s && _.isString(s)) {
+            return parser.parse(s);
+        } else {
+            return {
+                error: "Expected an HL7 message but got '" + s + "'"
+            }
+        }
+    },
+    formatDate: function(d) {
+        return moment(d).format('YYYYMMDDHHmmss');
     }
-  },
-  formatDate: function(date) {
-    if (date == null) {
-      date = new Date();
-    }
-    date = new Date(date);
-    function num(d) {
-      return _s.lpad(d, 2, '0');
-    }
-    return '' + (date.getFullYear()) + (num(date.getMonth() + 1)) + (num(date.getDate())) + (num(date.getHours())) + (num(date.getMinutes())) + (num(date.getSeconds()));
-  }
 };
